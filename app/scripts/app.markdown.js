5e9424d93f9538fa0bf2b79f66fad2bb2143d7c1
David Enke <david.enke@zalari.de>
2015-11-17 11:16:37 +0100

    added markdown provider config and showdown extensions

--- app/scripts/app.markdown.js
+++ app/scripts/app.markdown.js
@@ -6,29 +6,12 @@
  * @description
  * # dresdenjsApp
  *
- * Icons of the application.
+ * Markdown configuration of the application.
  */
 angular
     .module('dresdenjsApp')
-    .config(function ($mdIconProvider) {
-        $mdIconProvider
-            .iconSet('action', 'assets/svg/action-icons.svg', 24)
-            .iconSet('alert', 'assets/svg/alert-icons.svg', 24)
-            .iconSet('av', 'assets/svg/av-icons.svg', 24)
-            .iconSet('communication', 'assets/svg/communication-icons.svg', 24)
-            .iconSet('content', 'assets/svg/content-icons.svg', 24)
-            .iconSet('device', 'assets/svg/device-icons.svg', 24)
-            .iconSet('editor', 'assets/svg/editor-icons.svg', 24)
-            .iconSet('file', 'assets/svg/file-icons.svg', 24)
-            .iconSet('hardware', 'assets/svg/hardware-icons.svg', 24)
-            .iconSet('icons', 'assets/svg/icons-icons.svg', 24)
-            .iconSet('image', 'assets/svg/image-icons.svg', 24)
-            .iconSet('maps', 'assets/svg/maps-icons.svg', 24)
-            .iconSet('navigation', 'assets/svg/navigation-icons.svg', 24)
-            .iconSet('notification', 'assets/svg/notification-icons.svg', 24)
-            .iconSet('social', 'assets/svg/social-icons.svg', 24)
-            .iconSet('toggle', 'assets/svg/toggle-icons.svg', 24)
-            .icon('social:github-alt', 'assets/svg/social-github-alt.svg')
-            .icon('social:facebook', 'assets/svg/social-facebook.svg')
-            .icon('social:twitter', 'assets/svg/social-twitter.svg');
+    .config(function (markdownConverterProvider) {
+        markdownConverterProvider.config({
+            extensions: ['github', 'table', 'additionalAttributes']
+        });
     });
