360ee10bdfdd487de1304b12026b1a27d2df5f8f
David Enke <david.enke@zalari.de>
2016-03-08 17:31:56 +0100

    removed log

--- app/scripts/directives/ddjsNextMeetUp.directive.js
+++ app/scripts/directives/ddjsNextMeetUp.directive.js
@@ -18,7 +18,6 @@ angular
                 $http
                     .jsonp('https://api.meetup.com/DresdenJS-io-JavaScript-User-Group/events?scroll=next_upcoming&photo-host=public&page=1&sig_id=197134912&sig=487da08abe555db4de460efc496e543dcd7c8f00&callback=JSON_CALLBACK')
                     .then(function (response) {
-                        console.log(response.data.data[0]);
                         element.html($filter('date')(response.data.data[0].time, "dd. MMMM yyyy 'um' HH:mm 'Uhr'"));
                         element.parent().removeClass('hide');
                     })
