6a4eb2f944917ac12bad46fdcfb8510c34ce9773
David Enke <david.enke@zalari.de>
2016-03-08 17:27:53 +0100

    fixed meetup api request

--- app/scripts/directives/ddjsNextMeetUp.directive.js
+++ app/scripts/directives/ddjsNextMeetUp.directive.js
@@ -16,10 +16,11 @@ angular
             scope: false,
             link: function (scope, element, attrs) {
                 $http
-                    .get('https://api.meetup.com/DresdenJS-io-JavaScript-User-Group/events?scroll=next_upcoming&photo-host=public&page=1&sig_id=197134912&sig=487da08abe555db4de460efc496e543dcd7c8f00')
+                    .jsonp('https://api.meetup.com/DresdenJS-io-JavaScript-User-Group/events?scroll=next_upcoming&photo-host=public&page=1&sig_id=197134912&sig=487da08abe555db4de460efc496e543dcd7c8f00&callback=JSON_CALLBACK')
                     .then(function (response) {
-                        console.log(response);
-                        element.html($filter(response[0].time, 'dd. MMMM YYYY hh:mm Uhr'))
+                        console.log(response.data.data[0]);
+                        element.html($filter('date')(response.data.data[0].time, "dd. MMMM yyyy 'um' HH:mm 'Uhr'"));
+                        element.parent().removeClass('hide');
                     })
                     .catch(function () {
                         element.parent().addClass('hide');
