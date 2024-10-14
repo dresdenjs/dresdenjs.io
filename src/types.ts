2c38862a4da29b8636792a66cf5fb78e0d228340
David Enke <david.enke@zalari.de>
2024-10-14 21:00:30 +0200

    fix: remove semicolon, align types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,4 @@
-import type { MarkdownInstance, MDXInstance } from 'astro';
+import type { MarkdownInstance } from 'astro';
 
-export type MeetupEvent =
-  | MarkdownInstance<Record<string, any>>
-  | MDXInstance<Record<string, any>>;
+export type MeetupEvent = MarkdownInstance<Record<string, any>>;
+// | MDXInstance<Record<string, any>>;
