73680b49c1cd617a920339354b2ed63e9a0c905f
David Enke <david.enke@zalari.de>
2024-10-14 21:00:01 +0200

    fix: date format

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,4 +1,9 @@
+import 'dayjs/locale/de';
 import dayjs, { type ConfigType as DateLike } from 'dayjs';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+
+dayjs.locale('de-DE');
+dayjs.extend(LocalizedFormat);
 
 export function formatDate(date: DateLike): string {
   return dayjs(date).format('L');
